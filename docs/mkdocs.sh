5dae3c2806c0433914b0a5b0c729de1cfa4cc72c
Chuck Bass <chuck@acrocad.net>
2025-03-21 06:24:15 -0700

    Partial Update to better file insertion.

--- docs/mkdocs.sh
+++ docs/mkdocs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+set -e
+trap 'echo "Error occured at &LINENO. Command: $BASH_COMMAND"' ERR
 
 python ../src/ten8t/cli/ten8t_cli.py --help > snippets/help.txt
 python ../src/ten8t/rich_ten8t/rich_demo.py > snippets/rich_demo.txt
-#python ../src/ten8t/cli/ten8t_cli.py  --pkg=../src/examples/file_system  > snippets/result.json
+(cd ../src/ten8t/cli && python ten8t_cli.py --json=../../../docs/snippets/result.json  --pkg=../examples/file_system)
 radon cc --json   ../src/ten8t/t*.py > snippets/radon_cc.json
 radon mi --json   ../src/ten8t/t*.py > snippets/radon_mi.json
 radon hal --json  ../src/ten8t/t*.py > snippets/radon_hal.json
-python qc_rad.py
-#python insert_files.py ../README.md > ../README.MD
+python qc_radon.py
+python insert_files.py --output=../README.md ../README.md
 python add_badges.py
 make clean
 make html
